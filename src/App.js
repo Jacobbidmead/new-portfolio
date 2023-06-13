f48aedff8f808347c35fb6ea958a305db6d7ddb9
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2023-06-13 12:59:15 +0100

    Added main headline to home page

--- src/App.js
+++ src/App.js
@@ -5,8 +5,33 @@ function App() {
   return (
     <>
       <div className="main-page-container">
-        <div>hello world</div>
+        <div className="name-container">
+          <div id="home-text">
+            <div className="line line1">
+              <div className="word">HI, </div>
+              <p className="word">IM JACOB.</p>
+            </div>
+
+            <div className="line line2">
+              <p className="word">I</p>
+              <p className="word">MAKE THINGS</p>
+            </div>
+
+            <div className="line line3">
+              <p className="word">FOR </p>
+              <p className="word">THE WEB.</p>
+            </div>
+          </div>
+        </div>
         <Room />
+
+        <div>
+          {" "}
+          <img src="" alt="" />
+        </div>
+      </div>
+      <div className="">
+        <p className="word">WELCOME TO MY HOME</p>
       </div>
     </>
   );
