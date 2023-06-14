4c15e3337f9857bdfbca1e4e1492e10cdc502c2e
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2023-06-14 13:18:37 +0100

    Added main page text to textblur component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 // import Room from "./components/spline";
-
 import Nav from "./components/Nav.js";
 import "./App.css";
 import { useState, useEffect } from "react";
+import TextBlur from "./components/TextBlur.js";
 
 const colors = ["rgb(10, 26, 99)", "#FFC0CB", "#FFDAB9"];
 
@@ -26,24 +26,7 @@ const App = () => {
       >
         <Nav />
         <div className="contents-container">
-          <div className="name-container">
-            <div id="home-text">
-              <div className="line line1">
-                <div className="word">HI, </div>
-                <p className="word">IM JACOB.</p>
-              </div>
-
-              <div className="line line2">
-                <p className="word">I</p>
-                <p className="word">MAKE THINGS</p>
-              </div>
-
-              <div className="line line3">
-                <p className="word">FOR </p>
-                <p className="word">THE WEB.</p>
-              </div>
-            </div>
-          </div>
+          <TextBlur />
         </div>
       </div>
       <div className="welcome">
--- src/components/TextBlur.js
+++ src/components/TextBlur.js
@@ -1,16 +1,54 @@
-import { React, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import "../styles/TextBlur.css";
 
 const TextBlur = () => {
-  const [scrollPosition, setScrollPoistion] = useState(0);
+  const [scrollPosition, setScrollPosition] = useState(0);
   const [scrollDirection, setScrollDirection] = useState("up");
 
-  const handleScroll = () => {
-    const position = window.scrollY;
-    setScrollPoistion(positon > scrollPosition ? "down" : "up");
-    setScrollPoistion(postion);
-  };
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentPosition = window.scrollY;
+      setScrollDirection(currentPosition > scrollPosition ? "down" : "up");
+      setScrollPosition(currentPosition);
+    };
 
-  return <></>;
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [scrollPosition]);
+
+  return (
+    <>
+      {" "}
+      <div className={`text-container ${scrollPosition > 200 ? "blur" : ""}`}>
+        <div className="contents-container">
+          <div
+            className={`name-container ${
+              scrollDirection === "up" ? "front" : ""
+            }`}
+          >
+            <div id="home-text">
+              <div className="line line1">
+                <div className="word">HI, </div>
+                <p className="word">IM JACOB.</p>
+              </div>
+
+              <div className="line line2">
+                <p className="word">I</p>
+                <p className="word">MAKE THINGS</p>
+              </div>
+
+              <div className="line line3">
+                <p className="word">FOR </p>
+                <p className="word">THE WEB.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default TextBlur;
