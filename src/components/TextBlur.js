4f748daaceb3be632cfda99833d8c114e83a9db2
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2023-06-14 13:02:05 +0100

    Added handleScroll function

--- src/components/TextBlur.js
+++ src/components/TextBlur.js
@@ -1,6 +1,15 @@
 import { React, useState, useEffect } from "react";
 
 const TextBlur = () => {
+  const [scrollPosition, setScrollPoistion] = useState(0);
+  const [scrollDirection, setScrollDirection] = useState("up");
+
+  const handleScroll = () => {
+    const position = window.scrollY;
+    setScrollPoistion(positon > scrollPosition ? "down" : "up");
+    setScrollPoistion(postion);
+  };
+
   return <></>;
 };
 
