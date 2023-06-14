15583fcff9ab7d23a75b14751f45fdedc3e0d015
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2023-06-14 12:55:38 +0100

    Created TextBlur component
    will blur text on scroll

--- src/App.js
+++ src/App.js
@@ -4,14 +4,7 @@ import Nav from "./components/Nav.js";
 import "./App.css";
 import { useState, useEffect } from "react";
 
-const colors = [
-  "rgb(10, 26, 99)",
-  "#FFC0CB",
-  "#FFDAB9",
-  "#FFFFE0",
-  "#E0FFFF",
-  "#D8BFD8",
-];
+const colors = ["rgb(10, 26, 99)", "#FFC0CB", "#FFDAB9"];
 
 const App = () => {
   const [colorIndex, setColorIndex] = useState(0);
@@ -27,23 +20,28 @@ const App = () => {
   }, []);
   return (
     <>
-      <Nav />
-      <div className="main-page-container">
-        <div className="name-container">
-          <div id="home-text">
-            <div className="line line1">
-              <div className="word">HI, </div>
-              <p className="word">IM JACOB.</p>
-            </div>
+      <div
+        className="main-page-container"
+        style={{ backgroundColor: colors[colorIndex] }}
+      >
+        <Nav />
+        <div className="contents-container">
+          <div className="name-container">
+            <div id="home-text">
+              <div className="line line1">
+                <div className="word">HI, </div>
+                <p className="word">IM JACOB.</p>
+              </div>
 
-            <div className="line line2">
-              <p className="word">I</p>
-              <p className="word">MAKE THINGS</p>
-            </div>
+              <div className="line line2">
+                <p className="word">I</p>
+                <p className="word">MAKE THINGS</p>
+              </div>
 
-            <div className="line line3">
-              <p className="word">FOR </p>
-              <p className="word">THE WEB.</p>
+              <div className="line line3">
+                <p className="word">FOR </p>
+                <p className="word">THE WEB.</p>
+              </div>
             </div>
           </div>
         </div>
