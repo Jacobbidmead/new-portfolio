b8026f8241ed4e38cb98aec2e24b94bda52794e6
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2023-09-06 00:12:04 +0100

    Adjusted welcome fontsize & background color

--- src/App.js
+++ src/App.js
@@ -118,17 +118,25 @@ const App = () => {
             viewport={{ once: true, amount: 0.1 }}
             className="about"
           >
-            Im a creative front end developer with experience in{" "}
-            <span className="highlight-1">UX/UI design,</span>
-            <span className="highlight-2"> 3D imaging</span> and{" "}
-            <span className="highlight-3"> photography.</span> I build web
-            applications with TypeScript, React & Vue. My journey started with
-            haiku academys full stack web development bootcamp, which I
-            completed in october 2022. since graduating, I have learnt to
-            utilise my problem-solving skills to create intuative and responsive
-            web applications. I have a creative background, studying print &
-            time based media at UAL. A passionate photographer & explorer, I
-            have documented my travels around the world over the last ten years.
+            <p>
+              Im a creative front end developer with experience in{" "}
+              <span className="highlight-1">UX/UI design,</span>
+              <span className="highlight-2"> 3D imaging</span> and{" "}
+              <span className="highlight-3"> photography.</span> I build web
+              applications with TypeScript, React & Vue.{" "}
+            </p>
+            <p>
+              My journey started with haiku academys full stack web development
+              bootcamp, which I completed in october 2022. since graduating, I
+              have learnt to utilise my problem-solving skills to create
+              intuative and responsive web applications.
+            </p>
+            <p>
+              {" "}
+              I have a creative background, studying print & time based media at
+              UAL. A passionate photographer & explorer, I have documented my
+              travels around the world over the last ten years.
+            </p>
           </motion.div>
         </div>
       </div>
