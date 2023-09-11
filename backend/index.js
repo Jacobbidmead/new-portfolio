1d16fd78dd6d5880ad747b3b1f2c289f54490ef9
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2023-09-11 16:18:24 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -43,5 +43,5 @@ app.post("/send-email", async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
