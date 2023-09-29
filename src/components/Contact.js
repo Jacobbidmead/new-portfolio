b75edc9b60148f6113826dba2577fcc37389f042
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2023-09-29 15:16:24 +0100

    Made adjustments to contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,22 +3,48 @@ import { motion } from "framer-motion";
 import ReturnTop from "./ReturnTop";
 // import Asset from "./Asset";
 
+const isMobile = window.innerWidth < 601;
+
+const textAnimate = isMobile
+  ? {
+      offscreen: { y: 0 },
+      onscreen: { y: 0 },
+    }
+  : {
+      offscreen: { y: 200 },
+      onscreen: {
+        y: 0,
+        transition: {
+          type: "tween",
+          bounce: 0.5,
+          duration: 1,
+        },
+      },
+    };
+
 const Contact = () => {
   return (
     <section>
       <div className="contact-container">
-        <div className="contact-asset">{/* <Asset /> */}</div>
+        <motion.div
+          initial={"offscreen"}
+          whileInView={"onscreen"}
+          variants={textAnimate}
+          viewport={{ once: true, amount: 0.1 }}
+          className="contact-message"
+        >
+          <p>
+            Lets work togther; to discuss a project or comission, send me a
+            message.
+          </p>
+        </motion.div>
         <div className="contact">
           <form
             action="https://formsubmit.co/9cc5775898fe7b24b684bc331c8d80c2"
             method="POST"
           >
             <div className="contact-text">
-              <textarea
-                name="message"
-                placeholder="To discuss a project or comission, send me a message..."
-                required
-              />
+              <textarea name="message" placeholder="" required />
             </div>
             <div className="inputs">
               <div>
