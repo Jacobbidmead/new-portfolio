cea6734d07238ca3ca2f14ca5d2e3fe4d6c88d65
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2024-02-13 14:02:49 +0000

    Adjusted padding

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,22 +5,11 @@ const Footer = () => {
     <>
       {" "}
       <div className="footer">
-        <div className="name">jacobbidmead.com</div>
+        <div className="name">jacobbidmead.com.</div>
         <div className="nav-icons">
-          <div className="photo-link">
-            <a
-              href="https://open.spotify.com/playlist/3ieBiYfA1DqQMTOq2vpB0u?si=1780efef079247b1"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <span
-                className="hover-underline-animation"
-                style={{ color: "rgb(198, 198, 198)" }}
-              >
-                Spotify
-              </span>
-            </a>
-          </div>
+          <Link to="/Photo" className="photo-link">
+            <span className="hover-underline-animation"> Photos</span>
+          </Link>
           <div className="photo-link">
             <a
               href="https://github.com/Jacobbidmead"
@@ -36,9 +25,20 @@ const Footer = () => {
             </a>
           </div>
 
-          <Link to="/Photo" className="photo-link">
-            <span className="hover-underline-animation"> Photos</span>
-          </Link>
+          <div className="photo-link">
+            <a
+              href="https://open.spotify.com/playlist/3ieBiYfA1DqQMTOq2vpB0u?si=1780efef079247b1"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <span
+                className="hover-underline-animation"
+                style={{ color: "rgb(198, 198, 198)" }}
+              >
+                Spotify
+              </span>
+            </a>
+          </div>
         </div>
       </div>
     </>
