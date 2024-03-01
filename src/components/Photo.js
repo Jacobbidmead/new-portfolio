bebbe940d3845bd1675cd4feaedcb70f828801ec
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2024-03-01 17:11:52 +0000

    Update Photo.js

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -9,10 +9,8 @@ import { motion } from "framer-motion";
 
 const saDescription =
   "South & Central America: September 2017 - March 2018. Countries exhibited: Peru, Bolivia, Colombia & Mexico ";
-const asiaDescription =
-  "Japan, Vietnam & South Korea: September - November 2019";
-const europeDescription =
-  "Life in Europe: 2014 - 2018 - London, Oslo, Palermo, Crete, Barcelona";
+const asiaDescription = "Japan, Vietnam & South Korea: September - November 2019";
+const europeDescription = "Life in Europe: 2014 - 2018 - London, Oslo, Palermo, Crete, Barcelona";
 
 const Photo = () => {
   const [largePhoto, setLargePhoto] = useState("images/asia/tracks.png");
@@ -70,45 +68,42 @@ const Photo = () => {
             whileTap={{ scale: 0.9 }}
             transition={{ type: "spring", stiffness: 400, damping: 17 }}
             onClick={changeToAsia}
-            className="change-photos"
-          >
+            className="change-photos">
             Far East
           </motion.div>
           <motion.div
             whileTap={{ scale: 0.9 }}
             transition={{ type: "spring", stiffness: 400, damping: 17 }}
             onClick={changeToSa}
-            className="change-photos"
-          >
+            className="change-photos">
             Americas
           </motion.div>
           <motion.div
             whileTap={{ scale: 0.9 }}
             transition={{ type: "spring", stiffness: 400, damping: 17 }}
             onClick={changeToEurope}
-            className="change-photos"
-          >
+            className="change-photos">
             Europe
           </motion.div>
         </div>
         <div className="quote">
-          "Travel changes you, as you move through this life and this world you
-          change things slightly, you leave marks behind, however small. And in
-          return, life — and travel — leaves marks on you."{" "}
-          <p> - Anthony Bourdain</p>{" "}
+          "Travel changes you, as you move through this life and this world you change things
+          slightly, you leave marks behind, however small. And in return, life — and travel — leaves
+          marks on you." <p> - Anthony Bourdain</p>{" "}
           <p className="quote-break">
-            Over the last 10 years, I've been shooting street photography.
-            Capturing moments from my life & travels; cultures, experiences,
-            landscapes and compositions.
+            Over the last 10 years, I've been shooting street photography. Capturing moments from my
+            life & travels; cultures, experiences, landscapes and compositions.
           </p>
           <p className="quote-break">
             {" "}
-            Through my work, I attempt to describe my love of anthropology &
-            discovering new cultures.
+            Through my work, I attempt to describe my love of anthropology & discovering new
+            cultures.
           </p>
           <small>Shot on Contax TVS III</small>
         </div>
-        <ReturnTop />
+        <div style={{ display: "flex", justifyContent: "flex-end" }}>
+          <ReturnTop />
+        </div>
       </div>
     </>
   );
