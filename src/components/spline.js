a6eed8b2ed16615025124a3d814cef76d9e8f8c5
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2024-03-04 10:42:37 +0000

    removed unused files

--- src/components/spline.js
+++
@@ -1,7 +0,0 @@
-import Spline from "@splinetool/react-spline";
-
-export default function Room() {
-  return (
-    <Spline scene="https://draft.spline.design/I06nnBOHxaPNGs4g/scene.splinecode" />
-  );
-}
